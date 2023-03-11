db897d4ca9951e99004729af85001181158f145d
netudgi <netudgi@gmail.com>
2023-03-11 11:47:13 +0100

    netbeans commit

---
+++ src/gameoflife/GameOfLife.java
@@ -0,0 +1,10 @@
+package gameoflife;
+
+public class GameOfLife {
+
+    public static void main(String[] args) {
+        System.out.println("Let's do it again from scratch!");
+        
+    }
+    
+}
